bf025b244b6426f0d2e48b50bdd5dba2303aecde
Minhyun Ahn <alsgus2ek@naver.com>
2023-09-15 13:34:43 +0900

    feat :  db connect

--- config/db/db.config.ts
+++ config/db/db.config.ts
@@ -1,12 +1,14 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
-export const dbConfig: TypeOrmModuleOptions = {
-  type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: '1234',
-  database: 'tdd-test',
-  entities: [],
-  synchronize: true,
+export const dbConfig = (): TypeOrmModuleOptions => {
+  return {
+    type: 'postgres',
+    host: process.env.DATABASE_HOST,
+    port: +process.env.DATABASE_PORT,
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    entities: [],
+    synchronize: true,
+  };
 };
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { dbConfig } from 'config/db/db.config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(dbConfig)],
+  imports: [ConfigModule.forRoot(), TypeOrmModule.forRoot(dbConfig())],
   controllers: [AppController],
   providers: [AppService],
 })
