769d6b9006f92831d1a4910e8189f8c427a37dbb
MinhyunAhn <mhahn@crossteam.co.kr>
2023-09-18 16:01:26 +0900

    feat : user module repository pattern

---
+++ src/user/user.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { PrismaService } from 'prisma/prisma.service';
+import { UserController } from './controller/user.controller';
+import { UserRepository } from './repository/user.repository';
+import { UserService } from './service/user.service';
+
+@Module({
+  imports: [],
+  providers: [UserRepository, UserService, PrismaService],
+  controllers: [UserController],
+})
+export class UserModule {}
