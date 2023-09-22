f7ee4f6e6a03200800ff56cb2250e97ed4859f86
Minhyun Ahn <alsgus2ek@naver.com>
2023-09-22 16:50:23 +0900

    feat : add user log repository

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/service/prisma.service';
 import { UserController } from './controller/user.controller';
+import { UserLogRepository } from './repository/user-log.repository';
 import { UserRepository } from './repository/user.repository';
 import { UserService } from './service/user.service';
 
 @Module({
   imports: [],
-  providers: [UserRepository, UserService, PrismaService],
+  providers: [UserRepository, UserService, UserLogRepository, PrismaService],
   controllers: [UserController],
 })
 export class UserModule {}
