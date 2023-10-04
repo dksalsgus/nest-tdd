60e3713903f19254e6ba8b5d88bb04e5bd5005c8
dksalsgus <alsgus2ek@naver.com>
2023-10-04 17:03:46 +0900

    feat : global module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
 import { PrismaService } from './prisma/service/prisma.service';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), UserModule],
+  imports: [ConfigModule.forRoot(), UserModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { PrismaService } from 'src/prisma/service/prisma.service';
 import { UserController } from './controller/user.controller';
 import { UserLogRepository } from './repository/user-log.repository';
 import { UserRepository } from './repository/user.repository';
@@ -7,7 +6,7 @@ import { UserService } from './service/user.service';
 
 @Module({
   imports: [],
-  providers: [UserRepository, UserService, UserLogRepository, PrismaService],
+  providers: [UserRepository, UserService, UserLogRepository],
   controllers: [UserController],
 })
 export class UserModule {}
