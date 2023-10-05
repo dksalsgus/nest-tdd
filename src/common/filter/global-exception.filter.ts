98b366275260a4464985515e4548f0e04066c951
dksalsgus <alsgus2ek@naver.com>
2023-10-05 17:12:54 +0900

    feat : global exception filter 적용

--- src/common/filter/global-exception.filter.ts
+++ src/common/filter/global-exception.filter.ts
@@ -1,5 +1,9 @@
-import { ArgumentsHost, ExceptionFilter } from '@nestjs/common';
+import { ArgumentsHost, Catch, ExceptionFilter } from '@nestjs/common';
+import { GlobalException } from '../exception/global-exeption';
 
+@Catch(GlobalException)
 export class GlobalExceptionFilter implements ExceptionFilter {
-  catch(exception: any, host: ArgumentsHost) {}
+  catch(exception: any, host: ArgumentsHost) {
+    console.log('global exception filter ', exception);
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
+import { GlobalExceptionFilter } from './common/filter/global-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useGlobalFilters(new GlobalExceptionFilter());
+
   app.enableShutdownHooks();
   await app.listen(3000);
 }
