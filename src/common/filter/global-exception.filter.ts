dfc13ecc8ce91f8fc5c8842c341acc8628b2a894
dksalsgus <alsgus2ek@naver.com>
2023-10-05 17:17:14 +0900

    feat : exception type

--- src/common/filter/global-exception.filter.ts
+++ src/common/filter/global-exception.filter.ts
@@ -3,7 +3,7 @@ import { GlobalCustomError } from '../exception/global-exeption';
 
 @Catch(GlobalCustomError)
 export class GlobalExceptionFilter implements ExceptionFilter {
-  catch(exception: any, host: ArgumentsHost) {
+  catch(exception: GlobalCustomError, host: ArgumentsHost) {
     console.log('global exception filter ', exception);
   }
 }
