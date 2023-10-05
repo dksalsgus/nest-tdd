f320e95c672b387a468f17b03ff81982617ab0d4
dksalsgus <alsgus2ek@naver.com>
2023-10-05 17:24:02 +0900

    feat : ErrorCode

--- src/common/exception/global-exeption.ts
+++ src/common/exception/global-exeption.ts
@@ -1,5 +1,11 @@
 export class GlobalCustomException extends Error {
-  constructor(private readonly errorMessage: string) {
+  constructor(
+    private readonly errorCode: EnErrorCode,
+    private readonly errorMessage: string,
+  ) {
     super(errorMessage);
   }
+}
+export enum EnErrorCode {
+  userNotFound = 'USR001',
 }
