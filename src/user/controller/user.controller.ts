f4ba7943a8f4a9548c4e4827ac878c087a5a9dd5
dksalsgus <alsgus2ek@naver.com>
2023-10-11 14:35:04 +0900

    feat : api tags

--- src/user/controller/user.controller.ts
+++ src/user/controller/user.controller.ts
@@ -1,8 +1,10 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { RequestCreateUser } from '../model/request/request.create-user';
 import { UserService } from '../service/user.service';
 
 @Controller('/users')
+@ApiTags('User')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
