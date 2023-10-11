bd43e0c8340e4ee4d37860f959ce91e17b823d09
dksalsgus <alsgus2ek@naver.com>
2023-10-11 14:42:36 +0900

    feat : add api property

--- src/user/model/request/request.create-user.ts
+++ src/user/model/request/request.create-user.ts
@@ -1,5 +1,10 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class RequestCreateUser {
+  @ApiProperty({ description: '이메일', example: 'test@test.com' })
   email: string;
+  @ApiProperty({ description: '이름', example: '홍길동' })
   name: string;
+  @ApiProperty({ description: '패스워드', example: 'honggildong1234' })
   password: string;
 }
