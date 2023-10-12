4da6002d1f20ecb1b408bbd437024979d04e1c55
dksalsgus <alsgus2ek@naver.com>
2023-10-12 15:20:41 +0900

    feat : oauth controller

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { LoginOauthController } from './controller/login-oauth.controller';
 import { LoginController } from './controller/login.controller';
 import { AuthService } from './service/auth.service';
 
 @Module({
   imports: [],
   providers: [AuthService],
-  controllers: [LoginController],
+  controllers: [LoginController, LoginOauthController],
 })
 export class AuthModule {}
--- src/auth/controller/login.controller.ts
+++ src/auth/controller/login.controller.ts
@@ -1,35 +1,8 @@
-import { Controller, Get, Query, Res } from '@nestjs/common';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
-import { Response } from 'express';
-import { RequestNaverCallBackQuery } from '../model/request/request.naver-callback-query';
-import { ResponseNaverLogin } from '../model/response/response.naver-login';
+import { Controller } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { AuthService } from '../service/auth.service';
 @Controller('/login')
 @ApiTags('Login')
 export class LoginController {
   constructor(private readonly authService: AuthService) {}
-
-  @Get('/oauth/naver')
-  @ApiOperation({ summary: '네이버 Oauth' })
-  async naverLogin(@Res() res: Response): Promise<void> {
-    const result = await this.authService.naverLoginAuth();
-    return res.redirect(result);
-  }
-
-  @Get('/oauth/naver/callback')
-  @ApiOperation({ summary: '네이버 Oauth CallBack' })
-  async naverLoginCallback(
-    @Query() requestNaverCallBackQuery: RequestNaverCallBackQuery,
-  ): Promise<ResponseNaverLogin> {
-    const result = await this.authService.getNaverToken(
-      requestNaverCallBackQuery,
-    );
-    const {
-      access_token: accessToken,
-      expires_in: expiresIn,
-      refresh_token: refreshToken,
-      token_type: tokenType,
-    } = result;
-    return { accessToken, refreshToken, tokenType, expiresIn };
-  }
 }
