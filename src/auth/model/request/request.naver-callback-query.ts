5ab74aa60b9c29d4320a38c918ba705a81198a68
dksalsgus <alsgus2ek@naver.com>
2023-10-12 15:41:24 +0900

    docs : swagger update

--- src/auth/model/request/request.naver-callback-query.ts
+++ src/auth/model/request/request.naver-callback-query.ts
@@ -1,4 +1,23 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class RequestNaverCallBackQuery {
+  @ApiProperty({
+    description: `네이버 로그인 인증에 성공하면 반환받는 인증 코드, 접근 토큰(access token) 발급에 사용`,
+  })
   code: string;
+  @ApiProperty({
+    description:
+      '사이트 간 요청 위조 공격을 방지하기 위해 애플리케이션에서 생성한 상태 토큰으로 URL 인코딩을 적용한 값',
+  })
   state: string;
+  @ApiProperty({
+    description: `네이버 로그인 인증에 실패하면 반환받는 에러 코드
+  `,
+  })
+  error: string;
+  @ApiProperty({
+    description: `네이버 로그인 인증에 실패하면 반환받는 에러 메시지
+  `,
+  })
+  error_description: string;
 }
--- src/auth/model/response/response.naver-login.ts
+++ src/auth/model/response/response.naver-login.ts
@@ -2,22 +2,22 @@ import { ApiProperty } from '@nestjs/swagger';
 
 export class ResponseNaverLogin {
   @ApiProperty({
-    description: '엑세스 토큰',
+    description: `접근 토큰, 발급 후 expires_in 파라미터에 설정된 시간(초)이 지나면 만료됨`,
     example: 'access_tokenadsfsafsda',
   })
   accessToken: string;
   @ApiProperty({
-    description: '리프레쉬 토큰',
+    description: `갱신 토큰, 접근 토큰이 만료될 경우 접근 토큰을 다시 발급받을 때 사용`,
     example: 'refreshtoken',
   })
   refreshToken: string;
   @ApiProperty({
-    description: '토큰 타입',
+    description: '접근 토큰의 타입으로 Bearer와 MAC의 두 가지를 지원',
     example: 'bearer',
   })
   tokenType: string;
   @ApiProperty({
-    description: '토큰 만료시간',
+    description: `접근 토큰의 유효 기간(초 단위)`,
     example: '3600',
   })
   expiresIn: string;
