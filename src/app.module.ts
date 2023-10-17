cd9b61b05236287ecd8d0ce8251ce8a7b961b5d7
dksalsgus <alsgus2ek@naver.com>
2023-10-17 16:27:21 +0900

    feat : Joi 환경변수 유효성 검사

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
@@ -10,7 +11,16 @@ import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({
+      envFilePath: process.env.NODE_ENV === 'prod' ? '.env.prod' : '.env.dev',
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string().valid('prod', 'dev', 'debug'),
+        SERVER_URL: Joi.string().not().empty(),
+        DATABASE_URL: Joi.string().not().empty(),
+        NAVER_CLIENT_ID: Joi.string().not().empty(),
+        NAVER_SECRET: Joi.string().not().empty(),
+      }),
+    }),
     CommonModule,
     UserModule,
     PrismaModule,
