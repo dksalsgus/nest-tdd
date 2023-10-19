7c793c6a529386febead31f75e379c033e9240e9
dksalsgus <alsgus2ek@naver.com>
2023-10-19 15:07:06 +0900

    feat : user oauth create

---
+++ src/user/repository/user-oauth.repository.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { UserOauth } from '@prisma/client';
+import {
+  PrismaService,
+  PrismaTransaction,
+} from '../../prisma/service/prisma.service';
+
+@Injectable()
+export class UserOauthRepository {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async create(createData: Create, trx: PrismaTransaction): Promise<number> {
+    const query = trx.userOauth.create({
+      data: createData,
+    });
+    const result = await query;
+    return result.id;
+  }
+}
+
+type Create = Pick<UserOauth, 'service_name'>;
